0bf06dfa038458e55fbf3447448f9fee4dc6f0e9
wildgolem <105451121+wildgolem@users.noreply.github.com>
2025-05-20 23:30:35 -0400

    update api.js

--- api.js
+++ api.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 async function api() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'], 
+  });
+
   const page = await browser.newPage();
 
   await page.goto('https://hikari.gg/new-episodes', {
